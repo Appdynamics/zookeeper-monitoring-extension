04c2ad73ec1b66910ebfdddc69df8aa459aea84d
Bhuvnesh Kumar <raghur171@gmail.com>
2018-02-08 11:31:54 -0800

    updated licenses on all files

--- src/main/java/com/appdynamics/extensions/zookeeper/config/ConfigUtil.java
+++ src/main/java/com/appdynamics/extensions/zookeeper/config/ConfigUtil.java
@@ -1,3 +1,11 @@
+/*
+ *   Copyright 2018. AppDynamics LLC and its affiliates.
+ *   All Rights Reserved.
+ *   This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *   The copyright notice above does not evidence any actual or intended publication of such source code.
+ *
+ */
+
 package com.appdynamics.extensions.zookeeper.config;
 
 import org.apache.log4j.Logger;
